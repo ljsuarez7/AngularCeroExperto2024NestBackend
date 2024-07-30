73692cf02d571551ddb50d74e6d60723aea169d8
Javier Suárez <ljsuarez7@gmail.com>
2024-07-30 16:59:43 +0200

    conexion bd, creacion de usuario en bd y login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    
+    try {
+      
+      const {password, ...userData} = createUserDto;
+            
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+
+      await newUser.save();
+
+      const {password:_, ...user} = newUser.toJSON();
+
+      return user;
+
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`${createUserDto.email} already exists!`)
+      }
+      throw new InternalServerErrorException('Sucedió un error');
+    }
+
+  }
+
+  async login(loginDto: LoginDto){
+
+    const {email, password} = loginDto;
+    
+    const user = await this.userModel.findOne({email});
+
+    if(!user) throw new UnauthorizedException('Not valid credentials - email');
+
+    if(!bcryptjs.compareSync(password, user.password)) throw new UnauthorizedException('Not valid credentials - password');
+
+    const {password:_, ...rest} = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+    }
+
   }
 
   findAll() {
